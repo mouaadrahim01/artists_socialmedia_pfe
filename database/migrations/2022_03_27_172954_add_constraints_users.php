2694f2fec0207e06dcc888374588abc1c9c94954
Mouaad <mouadrahim01@gmail.com>
2022-03-27 18:20:14 +0000

    contraint

---
+++ database/migrations/2022_03_27_172954_add_constraints_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->foreign('art_id')->references('id')->on('arts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['art_id']);
+        });
+    }
+};
