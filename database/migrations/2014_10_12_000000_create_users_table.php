9c66a1bbb7a8f578da28bd4c378a1b3a07c5de02
mouaadrahim01 <mouaadrahim01>
2022-04-05 01:10:56 +0000

    new update for profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',50);
+            $table->string('nmae',50);
             $table->string('prenom',50);
             $table->string('email',50)->unique();
             $table->string('password');
